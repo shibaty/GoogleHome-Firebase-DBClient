41336ae25e6e3d9f8a1047149f5371fde67ee449
shibaty <shibaty@gmail.com>
2018-01-28 04:43:33 +0900

    fixed log

--- src/index.ts
+++ src/index.ts
@@ -4,37 +4,44 @@ import * as firebase from "firebase";
 import * as request from "request";
 import * as config from "./config";
 
+// main_rountine
 const mainRoutine = () => {
-  console.log("mainRoutine is started.");
+  console.log("Firebase DB Client is started.");
 
   firebase.initializeApp(config.firebase);
 
   const db = firebase.database();
   const ref = db.ref("electronics");
 
+  let isFirst = true;
+
   ref.on('value', (dataSnapshot) => {
-    console.log("value changed.");
-    const target: string = dataSnapshot.child("target").val();
-    const action: string = dataSnapshot.child("action").val();
+    if (isFirst) {
+      // skip the first notification because it will definitely come.
+      isFirst = false;
+    } else {
+      const target: string = dataSnapshot.child("target").val();
+      const action: string = dataSnapshot.child("action").val();
 
-    if (target && action) {
-      console.log("target: " + target + " action: " + action);
+      if (target && action) {
+        console.log("target: " + target + " action: " + action);
 
-      const options = {
-        url: config.url,
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        json: {
-          "target": target,
-          "action": action
-        }        
-      };
+        const options = {
+          url: config.url,
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          json: {
+            "target": target,
+            "action": action
+          }        
+        };
 
-      request(options, (error, response, body) => {
-        console.log(response.statusCode + " " + body);
-      });
+        request(options, (error, response, body) => {
+          console.log(response.statusCode + " " + body);
+        });
+      }
     }
   });
 };
